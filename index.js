33c7e038f6bd6102ce9bffe307d7df76e87ce3bb
Salah Adawi <salah.adawi@outlook.com>
2022-08-22 18:32:56 +0300

    part 3.2

--- index.js
+++ index.js
@@ -26,9 +26,13 @@ let persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
