31f852e2f257e76830a555a84f02992a28dee5af
Salah Adawi <salah.adawi@outlook.com>
2022-08-22 18:37:21 +0300

    part 3.3

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person)
+        response.json([person])
+    response.status(404).end()
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
