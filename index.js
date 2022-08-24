cbf95e4b85e0a78b7c7e9ae3350de4167709afb3
Salah Adawi <salah.adawi@outlook.com>
2022-08-24 13:46:16 +0300

    part 3.17

--- index.js
+++ index.js
@@ -83,6 +83,18 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+        response.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
