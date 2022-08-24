fff5afaed7ad69a4a05de5d950521cb1865b30f7
Salah Adawi <salah.adawi@outlook.com>
2022-08-24 13:27:40 +0300

    part 3.15

--- index.js
+++ index.js
@@ -60,9 +60,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id).then(
+        result => {
+            response.status(204).end()
+        }
+    ).catch(error => {
+        console.error(error.message)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
